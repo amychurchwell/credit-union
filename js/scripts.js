06871fdcc9cbfe6713170de4383932e0954b8af4
Amy Churchwell and Jenn Trass <amy@>
2017-10-31 14:51:01 -0700

    debug savings and checking objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //backend//
-function Money(deposit, withdrawal, initial){
+function Money(deposit, withdrawal, initial, account){
   this.initial = initial;
   this.withdrawal = withdrawal;
   this.deposit = deposit;
+  this.account = account;
 }
 
 Money.prototype.credit = function(){
@@ -30,6 +31,7 @@ $(document).ready(function() {
     var deposit = parseInt($('#deposit').val());
     var withdrawal = parseInt($('#withdrawal').val());
     var initial = parseInt($('#initial').val());
+    var account = $('input[name="account"]:checked').val();
 
     if (isNaN(withdrawal)) {
       withdrawal = 0;
@@ -38,15 +40,25 @@ $(document).ready(function() {
     if (isNaN(deposit)) {
       deposit = 0;
     }
-
-    var newMoney = new Money(deposit, withdrawal, initial);
+    debugger;
+    var newMoney = new Money(deposit, withdrawal, initial, account);
+    var newChecking = new Money(deposit, withdrawal, initial, account);
 
     $('#result-container').slideDown("slow");
-    $('#result').text("Your balance is: " + newMoney.credit() + ".");
 
-    if ((newMoney.credit()) < 0){
-      $('.alert-danger').slideDown("slow");
+    if (account === 'savings'){
+      $('#result').text("Your balance is: " + newMoney.credit() + ".");
+    }else if (account === 'checking'){
+      $('#result-checking').text("Your balance is: " + newChecking.credit() + ".");
     }
+
+
+    if (((newMoney.credit()) < 0) || (newChecking.credit()) < 0){
+      $('.alert-danger').slideDown("slow");
+    } else {
+      $('.alert-danger').slideUp("slow");
+    }
+
     event.preventDefault();
 
   });
