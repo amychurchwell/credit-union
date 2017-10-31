593fad380b09bba717fefef7245e05a4e5d19b46
Amy Churchwell and Jenn Trass <jenn@>
2017-10-31 13:22:37 -0700

    debug deposit and withdrawal function

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,18 @@ function Money(deposit, withdrawal, initial){
 }
 
 Money.prototype.credit = function(){
-  if((withdrawal != 0) && (deposit.length === 0)){
-    return this.initial - this.withdrawal;
-  }else if((deposit != 0) && (withdrawal.length === 0)){
+
+  if(this.deposit !== 0) {
     return this.initial + this.deposit;
+  }else if(this.withdrawal !== 0) {
+     return this.initial - this.withdrawal;
   }else{
     alert('error');
   }
 }
+
+
+
 
 // Money.prototype.addMoney = function(){
 //   var balance = this.initial + this.deposit;
@@ -23,14 +27,22 @@ Money.prototype.credit = function(){
 //front-end//
 $(document).ready(function() {
   $("#form").submit(function(event) {
-    var deposit = $('#deposit').val();
-    var withdrawal = $('#withdrawal').val();
-    var initial = $('#initial').val();
+    var deposit = parseInt($('#deposit').val());
+    var withdrawal = parseInt($('#withdrawal').val());
+    var initial = parseInt($('#initial').val());
+debugger;
+    if (isNaN(withdrawal)) {
+      withdrawal = 0;
+    }
+
+    if (isNaN(deposit)) {
+      deposit = 0;
+    }
 
     var newMoney = new Money(deposit, withdrawal, initial);
 
 
-    $('#result').append("Your balance is: " + newMoney.subtract());
+    $('#result').text("Your balance is: " + newMoney.credit() + "!");
 
     event.preventDefault();
 
